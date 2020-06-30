567d71d32ce7894e7d5f0c724baf6ef276c43c9b
Ajay Prakash Nair <42165927+ajaynair710@users.noreply.github.com>
2020-06-30 20:11:13 +0530

    updated

--- portfolio app/lib/main.dart
+++ portfolio app/lib/main.dart
@@ -1,117 +1,286 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 30.0),
+              CircleAvatar(
+                radius: 100.0,
+                backgroundImage: AssetImage('images/APN.jpg'),
+              ),
+              SizedBox(height: 7.0),
+              Text(
+                "Ajay Prakash Nair",
+                style: TextStyle(
+                  fontFamily: 'Solway',
+                  fontSize: 30.0,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 3.0),
+              Text(
+                "Ahalia School of Engineering and Technology",
+                style: TextStyle(
+                  fontFamily: 'Merriweather',
+                  color: Colors.white,
+                  fontSize: 16.0,
+                ),
+              ),
+              Text(
+                "Palakkad",
+                style: TextStyle(
+                  fontFamily: 'Merriweather',
+                  fontSize: 16.0,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                "Python | Java | Flutter | SQL | ML",
+                style: TextStyle(
+                  fontFamily: 'Merriweather',
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                "Developer | Designer",
+                style: TextStyle(
+                  fontFamily: 'Merriweather',
+                  fontSize: 21.0,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                width: 200.0,
+                child: Divider(
+                  color: Colors.black,
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  launch("tel://9130616844");
+                },
+                child: Container(
+                  decoration:
+                  BoxDecoration(border: Border.all(color: Colors.white)),
+                  padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.call,
+                        size: 30.0,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 60.0,
+                      ),
+                      Text(
+                        "+91-9130616844",
+                        style: TextStyle(
+                          fontSize: 23.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  launch(
+                      'https://mail.google.com/mail/u/0/#inbox?compose=CllgCJvpZxKgpnmDxrXzlkZMNtJRlkvvSxhSscNkGMTlpWXhtgWpCJCpTNLWnpLTqprgdtVkRjq');
+                },
+                child: Container(
+                  decoration:
+                  BoxDecoration(border: Border.all(color: Colors.white)),
+                  padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        size: 30.0,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        "ajaynair710@gmail.com",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  launch('https://github.com/ajaynair710');
+                },
+                child: Container(
+                  decoration:
+                  BoxDecoration(border: Border.all(color: Colors.white)),
+                  padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.device_hub,
+                        size: 30.0,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 55.0,
+                      ),
+                      Text(
+                        "github.com/ajaynair710",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  launch('https://www.linkedin.com/in/ajayprakashnair');
+                },
+                child: Container(
+                  decoration:
+                  BoxDecoration(border: Border.all(color: Colors.white)),
+                  padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.domain,
+                        size: 30.0,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 50.0,
+                      ),
+                      Text(
+                        "linkedin.com/in/ajayprakashnair",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  launch('https://twitter.com/ajaynair710');
+                },
+                child: Container(
+                  decoration:
+                  BoxDecoration(border: Border.all(color: Colors.white)),
+                  padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.person,
+                        size: 30.0,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        "https://twitter.com/ajaynair710",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  launch(
+                      'https://instagram.com/zuper__mario?igshid=cfnbycdt4ewo');
+                },
+                child: Container(
+                  decoration:
+                  BoxDecoration(border: Border.all(color: Colors.white)),
+                  padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.image,
+                        size: 30.0,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 40.0,
+                      ),
+                      Text(
+                        "instagram.com/zuper__mario",
+                        style: TextStyle(
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(height: 80.0),
+                    SizedBox(width: 150),
+                    Text(
+                      " © Ajay Prakash Nair",
+                      style: TextStyle(
+                        fontFamily: 'Merriweather',
+                        fontSize: 16.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
